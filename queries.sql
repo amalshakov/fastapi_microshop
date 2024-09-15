f42aa59238722e6bc220b9adb1dbdb4fa8b6a48e
Alexander Malshakov <89681115653@mail.ru>
2024-09-15 11:36:57 +0300

    Add queries.sql (for test queries)

--- queries.sql
+++ queries.sql
@@ -73,28 +73,70 @@
 --        posts
 -- WHERE
 --        posts.user_id IN (2, 6, 7)
+-- SELECT
+--        profiles.first_name,
+--        profiles.last_name,
+--        profiles.bio,
+--        profiles.id,
+--        profiles.user_id,
+--        users_1.username,
+--        users_1.id AS id_1
+-- FROM
+--        profiles
+--        JOIN users ON users.id = profiles.user_id
+--        LEFT OUTER JOIN users AS users_1 ON users_1.id = profiles.user_id
+-- WHERE
+--        users.username = "sam"
+-- ORDER BY
+--        profiles.id
+-- SELECT
+--        posts.user_id AS posts_user_id,
+--        posts.title AS posts_title,
+--        posts.body AS posts_body,
+--        posts.id AS posts_id
+-- FROM
+--        posts
+-- WHERE
+--        posts.user_id IN (2)
+-- SELECT
+--        orders.promocode,
+--        orders.created_at,
+--        orders.id
+-- FROM
+--        orders
+-- ORDER BY
+--        orders.id
+-- SELECT
+--        orders_1.id AS orders_1_id,
+--        products.name AS products_name,
+--        products.description AS products_description,
+--        products.price AS products_price,
+--        products.id AS products_id
+-- FROM
+--        orders AS orders_1
+--        JOIN order_product_association AS order_product_association_1 ON orders_1.id = order_product_association_1.order_id
+--        JOIN products ON products.id = order_product_association_1.product_id
+-- WHERE
+--        orders_1.id IN (1, 2)
 SELECT
-       profiles.first_name,
-       profiles.last_name,
-       profiles.bio,
-       profiles.id,
-       profiles.user_id,
-       users_1.username,
-       users_1.id AS id_1
+       orders.promocode,
+       orders.created_at,
+       orders.id
 FROM
-       profiles
-       JOIN users ON users.id = profiles.user_id
-       LEFT OUTER JOIN users AS users_1 ON users_1.id = profiles.user_id
-WHERE
-       users.username = "sam"
+       orders
 ORDER BY
-       profiles.id
+       orders.id
 SELECT
-       posts.user_id AS posts_user_id,
-       posts.title AS posts_title,
-       posts.body AS posts_body,
-       posts.id AS posts_id
+       order_product_association.order_id AS order_product_association_order_id,
+       order_product_association.id AS order_product_association_id,
+       order_product_association.product_id AS order_product_association_product_id,
+       order_product_association.count AS order_product_association_count,
+       products_1.name AS products_1_name,
+       products_1.description AS products_1_description,
+       products_1.price AS products_1_price,
+       products_1.id AS products_1_id
 FROM
-       posts
+       order_product_association
+       LEFT OUTER JOIN products AS products_1 ON products_1.id = order_product_association.product_id
 WHERE
-       posts.user_id IN (2)
+       order_product_association.order_id IN (1, 2)
