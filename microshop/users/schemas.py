e7a32af10f4c93d20210384b79d8e00ac7bef6bf
Alexander Malshakov <89681115653@mail.ru>
2024-09-19 10:11:10 +0300

    Add UserSchema

--- microshop/users/schemas.py
+++ microshop/users/schemas.py
@@ -1,9 +1,18 @@
 from typing import Annotated
 
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MinLen(3), MaxLen(20)]
     email: EmailStr
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    email: EmailStr | None = None
+    active: bool = True
