3b1a30c3a6366f1a3270e8e1cfe214b6eb6e7d31
Alexander Malshakov <89681115653@mail.ru>
2024-09-28 11:05:03 +0300

    Change validation.py

--- microshop/api_v1/demo_auth/validation.py
+++ microshop/api_v1/demo_auth/validation.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Any
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -34,18 +34,16 @@ def validate_token_type(
     payload: dict,
     token_type: str,
 ) -> bool:
-    pass
+    current_token_type = payload.get(TOKEN_TYPE_FIELD)
+    if current_token_type == token_type:
+        return True
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"invalid token type {current_token_type!r} expected {token_type!r}",
+    )
 
 
-def get_current_auth_user(
-    payload: Annotated[dict, Depends(get_current_token_payload)],
-) -> UserSchema:
-    token_type = payload.get(TOKEN_TYPE_FIELD)
-    if token_type != ACCESS_TOKEN_TYPE:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token type {token_type!r} expected {ACCESS_TOKEN_TYPE!r}",
-        )
+def get_user_by_token_sub(payload: dict) -> UserSchema:
     username: str | None = payload.get("sub")
     if user := user_db.get(username):
         return user
@@ -55,19 +53,29 @@ def get_current_auth_user(
     )
 
 
-def get_current_auth_user_for_refresh(
-    payload: Annotated[dict, Depends(get_current_token_payload)],
-) -> UserSchema:
-    token_type = payload.get(TOKEN_TYPE_FIELD)
-    if token_type != REFRESH_TOKEN_TYPE:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"invalid token type {token_type!r} expected {ACCESS_TOKEN_TYPE!r}",
-        )
-    username: str | None = payload.get("sub")
-    if user := user_db.get(username):
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="token invalid (user not found)",
-    )
+def get_auth_user_from_token_of_type(token_type: str):
+    def get_auth_user_from_token(
+        payload: Annotated[dict, Depends(get_current_token_payload)],
+    ) -> UserSchema:
+        validate_token_type(payload, token_type)
+        return get_user_by_token_sub(payload)
+
+    return get_auth_user_from_token
+
+
+class UserGetterFromToken:
+    def __init__(self, token_type: str):
+        self.token_type = token_type
+
+    def __call__(
+        self,
+        payload: Annotated[dict, Depends(get_current_token_payload)],
+    ):
+        validate_token_type(payload, self.token_type)
+        return get_user_by_token_sub(payload)
+
+
+get_current_auth_user = get_auth_user_from_token_of_type(ACCESS_TOKEN_TYPE)
+# get_current_auth_user = UserGetterFromToken(ACCESS_TOKEN_TYPE)
+
+get_current_auth_user_for_refresh = UserGetterFromToken(REFRESH_TOKEN_TYPE)
